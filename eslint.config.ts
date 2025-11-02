170241450f6baed4e20f0df1315f4428366982d4
Joshleeopard <Joshlee@live.com>
2025-11-01 23:32:45 -0700

    revert ignore

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,16 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-// TypeScript
 export default defineConfig([
-  {
-    ignores: ["dist/**"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
-  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
-])
+]);
